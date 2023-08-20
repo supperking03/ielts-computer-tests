1ca61572430971b296d4d3eb09d71471f71ef592
Kien <supperking03@gmail.com>
2023-08-20 23:08:22 +0700

    Add cam test 3 passage 1

--- src/components/data.js
+++ src/components/data.js
@@ -497,5 +497,114 @@ export const tests = [
             'outwards'
         ]
     },
+    {
+        id: 3,
+        title: 'Reading CAM 18 Test 3',
+        image: 'https://ieltstrainingonline.com/wp-content/uploads/2023/06/cam-18-test-2.jpg',
+        results:[],
+        passages: [
+            {
+                title: `Reading Passage 1`,
+                content: `
+                <p><span style="font-size: large;">You should spend about 20 minutes on </span><strong style="font-size: large;">Questions</strong><span style="font-size: large;"> </span><strong style="font-size: large;">1-13</strong><span style="font-size: large;">&nbsp;which are&nbsp;based on Reading Passage 1 below.</span><span style="font-size: 14px; text-align: left; color: #353535;"></span></p>
+                <h2 style="text-align: center;"><span style="font-size: x-large;"><strong>Materials to take us beyond concrete</strong></span></h2>
+                <p style="text-align: center;"><span style="font-size: large;"><em>Concrete is everywhere, but it’s bad for the planet, generating large amounts of carbon dioxide – alternatives are being developed</em></span></p>
+                <p><span style="font-size: large;"><strong>A</strong></span></p>
+                <p><span style="font-size: large;">Concrete is the second most used substance in the global economy, after water – and one of the world’s biggest single sources of greenhouse gas emissions. The chemical process by which cement, the key ingredient of concrete, is created results in large quantities of carbon dioxide. The UN estimates that there will be 9.8 billion people living on the planet by mid-century. They will need somewhere to live. If concrete is the only answer to the construction of new cities, then carbon emissions will soar, aggravating global warming. And so scientists have started innovating with other materials, in a scramble for alternatives to a universal commodity that has underpinned our modem life for many years.</span></p>
+                <p><span style="font-size: large;"><strong>B</strong></span></p>
+                <p><span style="font-size: large;">The problem with replacing concrete is that it is so very good at what it does. Chris Cheeseman, an engineering professor at Imperial College London, says the key thing to consider is the extent to which concrete is used around the world, and is likely to continue to be used. ‘Concrete is not a high-carbon product. Cement is high carbon, but concrete is not. But it is the scale on which it is used that makes it high carbon. The sheer scale of manufacture is so huge, that is the issue.’</span></p>
+                <p><span style="font-size: large;"><strong>C</strong></span></p>
+                <p><span style="font-size: large;">Not only are the ingredients of concrete relatively cheap and found in abundance in most places around the globe, the stuff itself has marvellous properties: Portland cement, the vital component of concrete, is mouldable and pourable, but quickly sets hard. Cheeseman also notes another advantage: concrete and steel have similar thermal expansion properties, so steel can be used to reinforce concrete, making it far stronger and more flexible as a building material than it could be on its own. According to Cheeseman, all these factors together make concrete hard to beat. ‘Concrete is amazing stuff. Making anything with similar properties is going to be very difficult.’</span></p>
+                <p><span style="font-size: large;"><strong>D </strong></span></p>
+                <p><span style="font-size: large;">A possible alternative to concrete is wood. Making buildings from wood may seem like a rather medieval idea, but climate change is driving architects to turn to treated timber as a possible resource. Recent years have seen the emergence of tall buildings constructed almost entirely from timber. Vancouver, Vienna and Brumunddal in Norway are all home to constructed tall, wooden buildings.</span></p>
+                <p><span style="font-size: large;"><strong>E</strong></span></p>
+                <p><span style="font-size: large;">Using wood to construct buildings, however, is not straightforward. Wood expands as it absorbs moisture from the air and is susceptible to pests, not to mention fire. But treating wood and combining it with other materials can improve its properties. Cross-laminated timber is engineered wood. An adhesive is used to stick layers of solid-sawn timber together, crosswise, to form building blocks. This material is light but has the strength of concrete and steel. Construction experts say that wooden buildings can be constructed at a greater speed than ones of concrete and steel and the process, it seems, is quieter.</span></p>
+                <p><span style="font-size: large;"><strong>F</strong></span></p>
+                <p><span style="font-size: large;">Stora Enso is Europe’s biggest supplier of cross-laminated timber, and its vice-president Markus Mannstrom reports that the company is seeing increasing demand globally for building in wood, with climate change concerns the key driver. Finland, with its large forests, where Stora Enso is based, has been leading the way, but the company is seeing a rise in demand for its timber products across the world, including in Asia. Of course, using timber in a building also locks away the carbon that it absorbed as it grew. But even treated wood has its limitations and only when a wider range of construction projects has been proven in practice will it be possible to see wood as a real alternative to concrete in constructing tall buildings.</span></p>
+                <p><span style="font-size: large;"><strong>G</strong></span></p>
+                <p><span style="font-size: large;">Fly ash and slag from iron ore are possible alternatives to cement in a concrete mix. Fly ash, a byproduct of coal-burning power plants, can be incorporated into concrete mixes to make up as much as 15 to 30% of the cement, without harming the strength or durability of the resulting mix. Iron-ore slag, a byproduct of the iron-ore smelting process, can be used in a similar way. Their incorporation into concrete mixes has the potential to reduce greenhouse gas emissions.</span></p>
+                <p><span style="font-size: large;">But Anna Surgenor, of the UK’s Green Building Council, notes that although these waste products can save carbon in the concrete mix, their use is not always straightforward. ‘It’s possible to replace the cement content in concrete with waste products to lower the overall carbon impact. But there are several calculations that need to be considered across the entire life cycle of the building- these include factoring in where these materials are being shipped from. If they are transported over long distances, using fossil fuels, the use of alternative materials might not make sense from an overall carbon reduction perspective.’</span></p>
+                <p><span style="font-size: large;"><strong>H </strong></span></p>
+                <p><span style="font-size: large;">While these technologies are all promising ideas, they are either unproven or based on materials that are not abundant. In their overview of innovation in the concrete industry, Felix Preston and Johanna Lehne of the UK’s Royal Institute of International Affairs reached the conclusion that, ‘Some novel cements have been discussed for more than a decade within the research community, without breaking through. At present, these alternatives are rarely as cost-effective as conventional cement, and they face raw-material shortages and resistance from customers.’</span></p>
+                <p><span style="font-size: large;"><strong></strong></span></p>
+                <p><span style="font-size: large;"><strong></strong></span></p>
+                
+                `,
+                question: `
+                <h3><em><span style="font-size: x-large;"><strong>Questions 1-4</strong></span></em></h3>
+                <p><span style="font-size: large;">Reading Passage 1 has eight sections, <strong>A-H</strong>.</span></p>
+                <p><span style="font-size: large;">Which section contains the following information?</span></p>
+                <p><span style="font-size: large;"><em>Write the correct letter, <strong>A-H</strong>, in boxes 1-4 on your answer sheet.</em></span></p>
+                <p><span style="font-size: large;"><strong></strong></span></p>
+                <p><span style="font-size: large;"><strong>1 </strong>&nbsp;&nbsp;an explanation of the industrial processes that create potential raw materials for concrete</span></p>
+                <p><span style="font-size: large;"><strong>2 </strong>&nbsp;&nbsp;a reference to the various locations where high-rise wooden buildings can be found</span></p>
+                <p><span style="font-size: large;"><strong>3 </strong>&nbsp;&nbsp;an indication of how widely available the raw materials of concrete are</span></p>
+                <p><span style="font-size: large;"><strong>4 </strong>&nbsp;&nbsp;the belief that more high-rise wooden buildings are needed before wood can be regarded as a viable construction material</span></p>
+                <p><span style="font-size: large;"><strong></strong></span></p>
+                <p><span style="font-size: large;"><strong></strong></span></p>
+                <h3><span style="font-size: x-large;"><em><strong>Questions 5-8</strong></em></span></h3>
+                <p><span style="font-size: large;"><em>Complete the summary below.</em></span></p>
+                <p><span style="font-size: large;"><em>Choose <strong>ONE WORD ONLY</strong> from the passage for each answer.</em></span></p>
+                <p><span style="font-size: large;"><em>Write your answers in boxes 5-8 on your answer sheet.</em></span></p>
+                <h3 style="text-align: center;"><span style="font-size: large;"><strong>Making buildings with wood</strong></span></h3>
+                <p><span style="font-size: large;">Wood is a traditional building material, but current environmental concerns are encouraging <strong>5</strong>………………. to use wood in modern construction projects. Using wood, however, has its challenges. For example, as <strong>6</strong>………………. in the atmosphere enters wood, it increases in size. In addition, wood is prone to pests and the risk of fire is greater. However, wood can be turned into a better construction material if it is treated and combined with other materials. In one process, <strong>7</strong>………………. of solid wood are glued together to create building blocks. These blocks are lighter than concrete and steel but equal them in strength. Experts say that wooden buildings are an improvement on those made of concrete and steel in terms of the <strong>8</strong>………………. with which they can be constructed and how much noise is generated by the process.</span></p>
+                <p><span style="font-size: large;"><strong></strong></span></p>
+                <p><span style="font-size: large;"><strong></strong></span></p>
+                <h3><em><span style="font-size: x-large;"><strong>Questions 6-13</strong></span></em></h3>
+                <p><span style="font-size: large;"><em>Look at the following statements (Questions 9-13) and the list of people below.</em></span></p>
+                <p><span style="font-size: large;"><em>Match each statement with the correct person, <strong>A</strong>, <strong>B</strong>, <strong>C</strong> or <strong>D</strong>.</em></span></p>
+                <p><span style="font-size: large;"><em>Write the correct letter, <strong>A</strong>, <strong>B</strong>, <strong>C</strong> or <strong>D</strong>, in boxes 9-13 on your answer sheet.</em></span></p>
+                <p><span style="font-size: large;"><strong><em>NB</em></strong><em>&nbsp;&nbsp; You may use any letter more than once.</em></span></p>
+                <p><span style="font-size: large;"><strong></strong></span></p>
+                <p><span style="font-size: large;"><strong>9</strong> &nbsp;&nbsp;The environmental advantage of cement alternatives may not be as great as initially assumed.</span></p>
+                <p><span style="font-size: large;"><strong>10</strong>&nbsp; &nbsp;It would be hard to create a construction alternative to concrete that offers so many comparable benefits.</span></p>
+                <p><span style="font-size: large;"><strong>11</strong> &nbsp;&nbsp;Worries about the environment have led to increased interest in wood as a construction material.</span></p>
+                <p><span style="font-size: large;"><strong>12</strong> &nbsp;&nbsp;Expense has been a factor in the negative response to the development of new cements.</span></p>
+                <p><span style="font-size: large;"><strong>13</strong>&nbsp; &nbsp;The environmental damage caused by concrete is due to it being produced in large quantities.</span></p>
+                <p style="padding-left: 80px;"><span style="font-size: large;"><strong>List of People</strong></span></p>
+                <p style="padding-left: 40px;"><span style="font-size: large;"><strong>A&nbsp; </strong>&nbsp;&nbsp;&nbsp;Chris Cheeseman</span></p>
+                <p style="padding-left: 40px;"><span style="font-size: large;"><strong>B&nbsp; </strong>&nbsp;&nbsp;&nbsp;Markus Mannstrom</span></p>
+                <p style="padding-left: 40px;"><span style="font-size: large;"><strong>C&nbsp; </strong>&nbsp;&nbsp;&nbsp;Anna Surgenor</span></p>
+                <p style="padding-left: 40px;"><span style="font-size: large;"><strong>D&nbsp; </strong>&nbsp;&nbsp;&nbsp;Felix Preston and Johanna Lehne</span></p>
+                <h2 style="text-align: center;"></h2>
+                
+                `
+            },
+            {
+                title: `Reading Passage 2`,
+                content: ``,
+                question: ``
+            },
+            {
+                title: `Reading Passage 3`,
+                content: ``,
+                question: ``
+            }
+        ]
+    }
+
     // ... add more tests
+    // {
+    //     id: 3,
+    //     title: 'Reading CAM 18 Test 3',
+    //     image: 'https://ieltstrainingonline.com/wp-content/uploads/2023/06/cam-18-test-2.jpg',
+    //     results:[],
+    //     passages: [
+    //         {
+    //             title: `Reading Passage 1`,
+    //             content: ``,
+    //             question: ``
+    //         },
+    //         {
+    //             title: `Reading Passage 2`,
+    //             content: ``,
+    //             question: ``
+    //         },
+    //         {
+    //             title: `Reading Passage 3`,
+    //             content: ``,
+    //             question: ``
+    //         }
+    //     ]
+    // }
 ];
