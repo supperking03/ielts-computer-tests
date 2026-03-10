4c0bf9d370e9df2c3feed3b6e05a0df3725e72c0
Kelvin <supperking03@gmail.com>
2026-03-10 20:27:16 +0700

    alo

--- src/components/dataGeneral/index.js
+++ src/components/dataGeneral/index.js
@@ -1,4 +1,3 @@
 import { generalTest01 } from './generalTest01';
-import { generalTest02 } from './generalTest02';
 
-export const tests = [generalTest01, generalTest02];
+export const tests = [generalTest01];
